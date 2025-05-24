d87ca0d37021b33ea056d4c981fd588415f8374b
Andrey <posandrey@gmail.com>
2025-05-24 10:37:25 +0200

    Fixing

--- infra/scripts/install.sh
+++ infra/scripts/install.sh
@@ -2,11 +2,11 @@ function certmanager() {
    helm upgrade --install certmanager ./infra/certmanager \
        --kubeconfig "${KUBECONFIG}" \
        --namespace certmanager \
-       --create-namespace
-       --set docker.pullSecret="${DOCKER_PULL_SECRET}" \
-       --set image.repository="${DOCKER_REPOSITORY}" \
-       --set image.tag="${DOCKER_TAG}" \
-       --set rev_id="${REV_ID}" \
+       --create-namespace \
+       --set-string docker.pullSecret="${DOCKER_PULL_SECRET}" \
+       --set-string image.repository="${DOCKER_REPOSITORY}" \
+       --set-string image.tag="${DOCKER_TAG}" \
+       --set-string rev_id="${REV_ID}" \
        -f ./infra/certmanager/values.yaml
 }
 
