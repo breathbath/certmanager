1929dbc3738cea67b5a1ee35e5b057c32ffbc21c
Andrey <posandrey@gmail.com>
2025-05-24 10:20:53 +0200

    Create namespace if it doesn exist

--- infra/scripts/install.sh
+++ infra/scripts/install.sh
@@ -2,6 +2,7 @@ function certmanager() {
    helm upgrade --install certmanager ./infra/certmanager \
        --kubeconfig "${KUBECONFIG}" \
        --namespace certmanager \
+       --create-namespace
        --set-string docker.pullSecret="${DOCKER_PULL_SECRET}" \
        --set-string image.repository="${DOCKER_REPOSITORY}" \
        --set-string image.tag="${DOCKER_TAG}" \
