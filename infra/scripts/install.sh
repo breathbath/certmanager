04219d1200957044eba62dac80995f663f066ef6
Andrey <posandrey@gmail.com>
2025-05-24 10:36:06 +0200

    Fixing

--- infra/scripts/install.sh
+++ infra/scripts/install.sh
@@ -3,10 +3,10 @@ function certmanager() {
        --kubeconfig "${KUBECONFIG}" \
        --namespace certmanager \
        --create-namespace
-       --set-string docker.pullSecret="${DOCKER_PULL_SECRET}" \
-       --set-string image.repository="${DOCKER_REPOSITORY}" \
-       --set-string image.tag="${DOCKER_TAG}" \
-       --set-string rev_id="${REV_ID}" \
+       --set docker.pullSecret="${DOCKER_PULL_SECRET}" \
+       --set image.repository="${DOCKER_REPOSITORY}" \
+       --set image.tag="${DOCKER_TAG}" \
+       --set rev_id="${REV_ID}" \
        -f ./infra/certmanager/values.yaml
 }
 
