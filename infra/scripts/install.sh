0470a58cc176bc4cb45d280582ecc63e04062c2c
Andrey <posandrey@gmail.com>
2025-08-31 01:47:42 +0200

    Env specific configuration

--- infra/scripts/install.sh
+++ infra/scripts/install.sh
@@ -5,6 +5,7 @@ function certmanager() {
        --create-namespace \
        --set-string docker.pullSecret="${DOCKER_PULL_SECRET}" \
        --set-string image.repository="${DOCKER_REPOSITORY}" \
+       --set-string configPath="${CONFIG_PATH}" \
        --set-string image.tag="${DOCKER_TAG}" \
        --set-string rev_id="${REV_ID}" \
        -f ./infra/certmanager/values.yaml
